a3f89963fd09c2703e556b2addc57992401d1792
FernandoRamirezD <fernandoramirezdelgado@gmail.com>
2026-06-25 19:14:28 -0300

    agregando test principales y carpeta admin

--- cypress/e2e/myTestRestfulBooker.cy.js
+++ cypress/e2e/myTestRestfulBooker.cy.js
@@ -1,34 +1,103 @@
-describe('Login perfil Admin', () => {
+describe('Reserva como Usuario Invitado', () => {
 
+  beforeEach(() => {
+    cy.intercept('GET', '**/api/room').as('rooms')
+    cy.visit('https://automationintesting.online/')
+    cy.wait('@rooms')
+  })
 
-  it('Login Exitoso', () => {
+  it('Reserva una habitación', () => {
+    // 1. Verificar home
+    //cy.contains('Shady Meadows B&B').should('be.visible')
+
+    //seleecionar fechas
+    cy.selecctRoom('17/07/2026', '27/07/2026')
+
+    //  Verificar habitaciones disponibles
+    cy.get('h5.card-title')
+      .should('have.length', 3)
+      .and('be.visible')
+
+    cy.get('.card-img-top')
+      .should('have.length', 3)
+
+    cy.get('p.card-text')
+      .should('have.length', 3)
+      .and('be.visible')
+
+    cy.get('.badge')
+      .should('have.length.at.least', 3)
+      .and('be.visible')
+
+    cy.contains('Book now').should('be.visible')
+
+    //  2. Seleccionar Double Room
+    cy.get('[href*="/reservation/2"]')
+      .click()
+
+    cy.url().should('include', '/reservation/2')
+
+    cy.get('h1').should('have.text', 'Double Room')
+    
+    cy.formReserva('Juan', 'Perez', 'juanperez@gmail.com', '29444554789')
+    
+    //validacion de confirmacion de reserva
+    cy.get('h2.card-title.fs-4.fw-bold.mb-3').should('contain', 'Booking Confirmed')
+  })
+
+  it('Formulario con campos vacios', () => {
+    cy.selecctRoom('17/07/2026', '27/07/2026')
+
+    //  Seleccionar Suite Room
+    cy.get('[href*="/reservation/3"]')
+      .click()
+
+    cy.url().should('include', '/reservation/3')
+
+    cy.get('h1').should('have.text', 'Suite Room')
+    
+    cy.formReserva(' ', ' ', ' ', ' ')
+
+    // Verificar que aparecen los mensajes de error correspondientes
+    cy.get('.alert').scrollIntoView().should('be.visible');
+    cy.get('.alert').and('be.visible')
+
+    cy.get('.alert').should('contain', 'Lastname should not be blank')
+      .and('contain', 'Firstname should not be blank')
+      .and('contain', 'size must be between 11 and 21')
+      .and('contain', 'size must be between 3 and 18')
+      .and('contain', 'size must be between 3 and 30')
+  })
+
+  it('verificar que no se realiza la reserva', () => {
+    cy.get(':nth-child(6) > .nav-link').click()
     cy.login('admin', 'password')
-    cy.log('Test Login Admin')
-    cy.contains('Restful Booker Platform Demo').should('be.visible');
+
+    cy.get('#reportLink').click()
+    cy.get(':nth-child(4) > .rbc-row-bg > :nth-child(4)').should('be.empty')
+    cy.get(':nth-child(4) > .rbc-row-bg > :nth-child(5)').should('be.empty')
+    cy.get(':nth-child(4) > .rbc-row-bg > :nth-child(6)').should('be.empty')
   })
 
-  it('Login con usuario incorrecto', () => {
-    cy.login('administrador', 'password')
-    cy.log('Test Login usuario Incorrecto')
-    cy.contains('Invalid credentials').should('be.visible');
-  })
-
-  it('Login con password incorrecto', () => {
-    cy.login('administrador', 'passw')
-    cy.log('Test Login Password Incorrecto')
-    cy.contains('Invalid credentials').should('be.visible');
-  })
-
-  it('Crear un cuarto con datos correctos', () => {
-    cy.login('admin', 'password')
-    cy.log('Test Nuevo Cuarto')
-    cy.newRooms('105', '555')
-    cy.get('[data-testid="roomlisting"]') // <--- Reemplaza por el selector real de la fila en esa web
-      .last()                 // Se posiciona estrictamente en la última de la lista
-      .within(() => {         // Limita la búsqueda solo a esa última fila
-        // Valida que los datos dentro de la última fila coincidan con lo creado
-        cy.contains('105').should('be.visible');
-        cy.contains('555').should('be.visible');
-      });
-  })
 })
+
+describe('Formulario de contacto', () =>{
+   
+    it('formulario de contacto con datos válidos', ()=>{
+// Completar el formulario de contacto con datos válidos
+       cy.visit('https://automationintesting.online/#contact')
+       cy.url().should('include', '#contact')
+       cy.contains('Send Us a Message')
+
+       cy.formContacto('Ana Lopez', 'analopezal@gmail.com', '29444554789', 'Consulta sobre reserva',
+         'Hola, Estoy interesado/a en realizar una reserva y quisiera recibir más información sobre la disponibilidad y los servicios incluidos. Muchas gracias Saludos.')
+       //2. Enviar el mensaje y validar que se muestra la confirmación
+        cy.get('.d-grid > .btn').click()
+
+        cy.get('.col-lg-8 > .card > .card-body > .h4')
+        .contains('Thanks for getting in touch')
+        .should('be.visible')
+
+    })
+
+} )
