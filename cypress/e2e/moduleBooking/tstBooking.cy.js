0b0f87453e3681829860a7279fc3667c302bcdf0
julioMartin12 <rl_yoo@yahoo.com.ar>
2026-06-25 07:59:33 -0300

    test: should verify the correct number of selected days

--- cypress/e2e/moduleBooking/tstBooking.cy.js
+++ cypress/e2e/moduleBooking/tstBooking.cy.js
@@ -17,8 +17,24 @@ describe('Booking Module', () => {
         cy.get('p.lead.text-muted').should('contain.text', 'Comfortable beds and delightful breakfast from locally sourced ingredients');
         cy.get('.btn').contains('Book now').first().click();
         cy.url().should('include', '/reservation');
-        
+
     });
-    
+
+
+    it('should verify the correct number of selected days', () => {
+        cy.searchAvailability(6);
+        cy.get('h2').should('contain.text', 'Our Rooms');
+        cy.get('p.lead.text-muted').should('contain.text', 'Comfortable beds and delightful breakfast from locally sourced ingredients');
+        cy.get('.btn').contains('Book now').first().click();
+
+        cy.url().should('include', '/reservation');
+        cy.get('span').contains('6 night').should('exist');
+
+        cy.verifyTotalPrice('Single', 6).then((totalPrice) => {
+            cy.contains('span', `£${totalPrice}`).should('be.visible');
+
+        });
+    });
+
 
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -16,3 +16,16 @@ Cypress.Commands.add('searchAvailability', (daysOfStay = 5) => {
 
     cy.get('button').contains('Check Availability').click();
 });
+
+
+Cypress.Commands.add('verifyTotalPrice', (roomType, daysOfStay) => {
+    const roomRates = {
+        'Single': 100,
+        'Double': 150,
+        'Suite': 225
+    };
+
+    const ratePerNight = roomRates[roomType] || 0;
+    
+    return ratePerNight * daysOfStay;
+});
